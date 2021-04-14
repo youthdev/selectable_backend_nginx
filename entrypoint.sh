810759d7e6f73b66169258822d327fa72ad37ad3
truong.hua <truong.hua@youthdev.net>
2021-04-14 17:18:32 +0700

    Add ability to config upstream base on request header instead of fixed envs, so that a proxy instance can be reuse for multiple services

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 
 sed -i -E "s/^;?worker_processes .*/worker_processes ${NGX_WORKER_PROCESSES:-10};/" /usr/local/openresty/nginx/conf/nginx.conf
 
-if [ ! -z "$QDB_MAX_BODY_SIZE" ]; then
-	sed -i -E "s/^(\s*)client_max_body_size .*/\1client_max_body_size ${QDB_MAX_BODY_SIZE};/" /etc/nginx/conf.d/default.conf
-	sed -i -E "s/^(\s*)client_body_buffer_size .*/\1client_body_buffer_size ${QDB_MAX_BODY_SIZE};/" /etc/nginx/conf.d/default.conf
+if [ ! -z "$RESOLVER" ]; then
+	sed -i -E "s/127.0.0.11/${RESOLVER};/" /etc/nginx/conf.d/default.conf
 fi
 
 exec "$@"
